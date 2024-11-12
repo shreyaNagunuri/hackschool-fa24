68a0ae7573b650840f949a630977fe6107c3c19b
nathanwang0114 <nxwang@ucsd.edu>
2024-11-12 13:14:34 -0800

    Added part5 starter code

---
+++ client/src/components/ActivityCard/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./style.module.css";
+
+const ActivityCard = ({ trip, activitySpot, rating, review }) => {
+    // Code here
+    {/* Bonus: express rating as stars using a cool trick with star emojis using fill and join string operations
+          Array(Number(rating)).fill('⭐').join('') */}
+};
+
+export default ActivityCard;
---
+++ client/src/components/TripCard/index.jsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "./style.module.css";
+
+const TripCard = ({ destination, startDate, endDate, journalEntry }) => {
+  //Code here
+};
+
+export default TripCard;
